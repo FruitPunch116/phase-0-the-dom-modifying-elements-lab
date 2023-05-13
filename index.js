4520743bbdfd61d3889498192c4913536344e819
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-05-13 09:23:12 -0400

    DOM Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML =
+"Satoshi is the champion";
+
+document.body.append(newHeader);
+
+//newHeader.textContent = "Satoshi is the champion";
+//console.log(newHeader.textContent);
+
+//newHeader = document.getElementById("victory");
