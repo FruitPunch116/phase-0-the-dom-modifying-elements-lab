8d434c5bbd98753ab829b691750b7dbd93f4cceb
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-05-23 15:07:23 -0400

    New code (Paired lab)

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove();
+const newHeader=document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent="Satoshi is the champion!!"
+document.body.append(newHeader)
+
+
+//doesn't work, even tough my partner's computer is working, worked just were in a diferfeent repo
